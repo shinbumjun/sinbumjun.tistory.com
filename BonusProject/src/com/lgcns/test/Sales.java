c864a978071bc6853ae4ee430edcddd39efed15b
user <user@DESKTOP-KQT1PD2>
2024-05-27 15:21:08 +0900

    2024-05-27(월) 자녀 객체를 인터페이스 타입으로 형 변환 + abstract 선언 이유?

---
+++ BonusProject/src/com/lgcns/test/Sales.java
@@ -0,0 +1,20 @@
+package com.lgcns.test;
+
+public class Sales extends Employee implements Bonus {
+
+    public Sales() {}
+
+    public Sales(String name, int number, String department, int salary) {
+        super(name, number, department, salary);
+    }
+
+    @Override
+    public double tax() {
+        return getSalary() * 0.13; // 세금 13%
+    }
+
+    @Override
+    public void incentive(int pay) {
+        setSalary(getSalary() + (int)(pay * 1.0));
+    }
+}
---
+++ BonusProject/src/com/lgcns/test/Secretary.java
@@ -0,0 +1,20 @@
+package com.lgcns.test;
+
+public class Secretary extends Employee implements Bonus {
+
+    public Secretary() {}
+
+    public Secretary(String name, int number, String department, int salary) {
+        super(name, number, department, salary);
+    }
+
+    @Override
+    public double tax() {
+        return getSalary() * 0.1; // 세금 10%
+    }
+
+    @Override
+    public void incentive(int pay) {
+        setSalary(getSalary() + (int)(pay * 0.8));
+    }
+}
